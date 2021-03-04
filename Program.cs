d5838863575d9c3e884aaad6909ae6a4e2208215
Rickie Le <rickienthefishies@gmail.com>
2021-03-04 14:50:32 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make it pretty 
+            Console.Title = "Adventure Quest";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+        }
+    }
+}
